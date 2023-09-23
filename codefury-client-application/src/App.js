c21d497efb7708c4d31a553d3c11694aaa118336
NITHEESHWAR B R <nitheeshwar.r.g@gmail.com>
2023-09-23 19:23:56 +0530

    Added Main Page and NAv Bar

--- codefury-client-application/src/App.js
+++ codefury-client-application/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import Authentication from './Pages/AuthenticationPage/Authentication';
+import { Routes, Route } from 'react-router-dom'
+import Authentication from './Pages/AuthenticationPage/Authentication'
+import AppContext from '../src/contexts/AppContext';
+import { useState } from 'react';
+import MainPage from './Pages/MainPage/MainPage';
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [user, setUser] = useState(null)
+
+  const globaldata = {
+    isLoggedIn, setIsLoggedIn,
+    user, setUser
+  };
   return (
-    <div className="App">
-     <Routes>
-      <Route path='/Auth' element={<Authentication/>}/>
-     </Routes>
-    </div>
+    <AppContext.Provider value={globaldata}>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<MainPage/>}/>
+          <Route path='/authentication' element={<Authentication/>} />
+          {/* <Route path='/online-harrasment' element={<OnlineHarrasment />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path='*' element={<ErrorPage />} /> */}
+        </Routes>
+
+      </div>
+    </AppContext.Provider>
   );
 }
 
---
+++ codefury-client-application/src/Components/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from '@mui/material/Link'
+import '../Components/NavBar.css'
+function NavBar() {
+    return (
+        <div className='acutal-nav'>
+            <div className='main-nav'>
+                <div className='navigation'>
+                    <div className='links-div'>
+                        <Link href="/Auth" underline="hover" className="Links">
+                            {'Authentication'}
+                        </Link>
+                        <Link href="/online-harassment" underline="hover" className="Links">
+                            {'Online Harassment'}
+                        </Link>
+                        <Link href="/chatBot" underline="hover" className="Links">
+                            {'ChatBot'}
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default NavBar
